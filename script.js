4e869de43dfb737ceddf8ca4a12a84a0c1f406cb
unknown <sourabh.rajput.22082001@gmail.com>
2023-10-13 18:11:38 +0530

    completed

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ const pauseButton = document.getElementById("pauseButton");
 const synth = window.speechSynthesis;
 let utterance = new SpeechSynthesisUtterance();
 
-// Set the default voice and volume
-utterance.voice = synth.getVoices()[0]; // You can choose a different voice if needed
-utterance.volume = 1; // Adjust the volume as needed (0 to 1)
 
-// Event listeners
+utterance.voice = synth.getVoices()[0]; 
+utterance.volume = 1; 
+
+
 playButton.addEventListener("click", () => {
     if (textToSpeak.value.trim() !== "") {
         utterance.text = textToSpeak.value;
-        synth.cancel(); // Stop any ongoing speech
+        synth.cancel(); 
         synth.speak(utterance);
     }
 });
@@ -22,8 +22,5 @@ pauseButton.addEventListener("click", () => {
     synth.pause();
 });
 
-// Resume button (if needed)
-resumeButton.addEventListener("click", () => {
-    synth.resume();
-});
+
 
